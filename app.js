db85a326e0dc744782170cc3fe27cfa9a757ff0a
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2025-08-05 18:04:55 -0600

    Implementacion de la funcion para agregar y mostrar amigos

--- app.js
+++ app.js
@@ -2,4 +2,36 @@
 // tus habilidades en lógica de programación.
 // Aquí deberás desarrollar la lógica para resolver
 // el problema.
-console.log("Hola mundo")
+let amigos = []
+
+//Funcion para agregar a un amigo.
+function agregarAmigo() {
+    const ingresarAmigo = document.getElementById('amigo');
+    const nombre = ingresarAmigo.value.trim();
+
+    //Para validar la entrada
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre ");
+        return;
+    }
+
+    //Añadimos los nombres de lo amigos dentro del array
+    amigos.push(nombre);
+
+    //Limpiamos el la cajita donde se ingresa el nombre
+    ingresarAmigo.value = "";
+
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+console.log(amigos);
